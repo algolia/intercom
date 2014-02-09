bdd70943300446859a812b7df4400348ff7ce554
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-09 11:01:35 -0800

    Force SSL + basic-auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  unless Rails.env.development?
+    force_ssl
+    http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD']
+  end
+
   def index
   end
 end
