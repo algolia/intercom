5a80d12f125c4dab6dcb9d1105dda519b06d34c2
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-08 16:54:55 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Intercom::Application.config.secret_key_base = '14b892e6cfd81a5e0b6a204abe32744c9c40c03bc075f1195745da12e29761f5a755eed7c02bfd5097b61db8d954b3191c8b0947dfb85074c713545b9e554364'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Intercom::Application.config.session_store :cookie_store, key: '_intercom_session'
