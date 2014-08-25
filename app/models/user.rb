7b7c47cbf7a2ec5f5de82ad0fce9c484827f54de
Sylvain UTARD <sylvain.utard@gmail.com>
2014-08-25 20:38:32 +0200

    Sanitize HTML

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User
     index.set_settings attributesToIndex: ['name', 'email', 'social_profiles.username', 'location_data.city_name', 'location_data.country_name'],
       customRanking: ['desc(session_count)'],
       queryType: 'prefixAll'
-    index.clear
+    sanitizer = HTML::FullSanitizer.new
     Intercom::User.all.each do |user|
       record = user.to_hash
       record['objectID'] = record.delete('intercom_id')
       record['social_profiles'] = user.social_profiles.map { |profile| JSON.parse(profile.to_json) }
       record['location_data'] = user.location_data
-      records << record
+      records << sanitize(record, sanitizer)
       if records.size > BATCH_SIZE
         index.add_objects records
         records = []
@@ -22,6 +22,21 @@ class User
     end
     index.add_objects records unless records.empty?
     Algolia.move_index index.name, ENV['ALGOLIA_INDEX']
+  end
+
+  private
+  def self.sanitize(v, sanitizer)
+    case v
+    when String
+      sanitizer.sanitize(v)
+    when Hash
+      v = v.dup # work-around frozen hash
+      v.each { |key, value| v[key] = sanitize(value, sanitizer) }
+    when Array
+      v.map { |x| sanitize(x, sanitizer) }
+    else
+      v
+    end
   end
 
 end
